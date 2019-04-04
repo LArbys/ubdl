19178a311754c31e8f8ddf31c49eb768f277ec19
kmason03 <masonkatie31@gmail.com>
2019-04-04 18:04:47 +0000

    bump ublarcvapp for infill cropper

--- scripts/tufts_start_container.sh
+++ scripts/tufts_start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/ubdl_singularity_031219.img
+container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_032919.img
 
 module load singularity
 singularity shell $container
