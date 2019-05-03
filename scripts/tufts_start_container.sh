eda11b9333b553f0e0746f5496ebee2d5aaf6af3
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2019-05-02 23:45:26 -0400

    update tufts container in script name

--- scripts/tufts_start_container.sh
+++ scripts/tufts_start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_032919.img
+container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_041519.img
 
 module load singularity
 singularity shell $container
