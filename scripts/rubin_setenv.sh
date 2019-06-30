11945d951b9f905697be7cda20502ae589536f12
twongjirad <taritree.wongjirad@gmail.com>
2019-06-30 09:32:37 -0400

    added rubin setenv

--- scripts/rubin_setenv.sh
+++ scripts/rubin_setenv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Specific locations for Tufts Rubin machine
+# Note locations here are some defaults that typically work for ubuntu.
+# they are also the locations used in the [ubdl container](https://github.com/larbys/larbys-containers)
 
 # ROOT
-#source /usr/local/root/build/bin/thisroot.sh
+source /usr/local/bin/thisroot.sh
 
 # CUDA
 # typical location of cuda in ubuntu
@@ -19,4 +20,9 @@ export OPENCV_LIBDIR=/usr/local/lib
 export LIBTORCH_DIR="/usr/local/lib/python2.7/dist-packages/torch"
 export LIBTORCH_LIBDIR=${LIBTORCH_DIR}/lib
 export LIBTORCH_INCDIR=${LIBTORCH_DIR}/lib/include
+#export LIBTORCH_DIR="/usr/local/torchlib"
+#export LIBTORCH_LIBDIR=${LIBTORCH_DIR}/lib
+#export LIBTORCH_INCDIR=${LIBTORCH_DIR}/include
+#[[ ":$LD_LIBRARY_PATH:" != *":${LIBTORCH_LIBDIR}:"* ]] && \
+#    export LD_LIBRARY_PATH="${LIBTORCH_LIBDIR}:${LD_LIBRARY_PATH}"
 
