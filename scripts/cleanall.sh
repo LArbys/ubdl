288ad36bc7ee6a3f9139befbf8f6e86c1136c5c5
Taritree Wongjirad <taritree@mit.edu>
2019-07-01 23:09:58 -0500

    modified clean script and ups table to v1_0_0

--- scripts/cleanall.sh
+++ scripts/cleanall.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-start_dir=$PWD
+__ubdl_cleanall_start_dir__=$PWD
 
 cd $UBDL_BASEDIR
 
+echo "<<< CLEAN LARLITE >>>"
 cd larlite
 make clean
 cd UserDev/BasicTool
 make clean
 cd $UBDL_BASEDIR
 
+echo "<<< CLEAN GEO2D >>>"
 cd Geo2D
 make clean
 cd $UBDL_BASEDIR
 
+echo "<<< CLEAN LAROPENCV >>>"
 cd LArOpenCV
 make clean
 cd $UBDL_BASEDIR
 
+echo "<<< CLEAN LARCV >>>"
 cd larcv/build && make clean
 cd $UBDL_BASEDIR
 
+echo "<<< CLEAN UBLARCVAPP >>>"
 cd ublarcvapp/build && make clean
 cd $UBDL_BASEDIR
 
+echo "<<< CLEAN LARFLOW >>>"
+cd larflow/build && make clean
+cd $UBDL_BASEDIR
+
 echo "cleaned ubdl repositories"
-cd $start_dir
+cd $__ubdl_cleanall_start_dir__
