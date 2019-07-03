6eec5cd71aef6586438e231842e3bf239c3e7761
twongjirad <taritree.wongjirad@gmail.com>
2019-07-03 02:09:45 -0400

    add larflow to builall py2 script

--- buildall_py2.sh
+++ buildall_py2.sh
@@ -6,19 +6,19 @@ build_log=${__ubdl_buildall_py2_workdir__}/build.log
 
 echo "<<< BUILD LARLITE >>>"
 cd larlite
-make >& $build_log
+make >& $build_log 2>&1
 cd UserDev/BasicTool
-make -j4 >>& $build_log
+make -j4 >> $build_log 2>&1
 cd $__ubdl_buildall_py2_workdir__
 
 echo "<<< BUILD GEO2D >>>"
 cd Geo2D
-make -j4 >>& $build_log
+make -j4 >> $build_log 2>&1
 cd $__ubdl_buildall_py2_workdir__
 
 echo "<<< BUILD LAROPENCV >>>"
 cd LArOpenCV
-make -j4 >>& $build_log
+make -j4 >> $build_log 2>&1
 cd $__ubdl_buildall_py2_workdir__
 
 echo "<<< BUILD LARCV >>>"
@@ -26,7 +26,7 @@ cd larcv
 mkdir -p build
 cd build
 cmake -DUSE_PYTHON2=ON -DUSE_OPENCV=ON -DON_FNAL=ON ../
-make install >>& $build_log
+make install >> $build_log 2>&1
 cd $__ubdl_buildall_py2_workdir__
 
 echo "<<< BUILD CILANTRO >>>"
@@ -34,7 +34,7 @@ cd cilantro
 mkdir -p build
 cd build
 cmake ../
-make >>& $build_log
+make >> $build_log 2>&1
 cd $__ubdl_buildall_py2_workdir__
 
 echo "<<< BUILD UBLARCVAPP >>>"
@@ -43,7 +43,16 @@ cd ublarcvapp
 source configure.sh
 cd build
 cmake -DUSE_OPENCV=ON ../
-make install >>& $build_log
+make install >> $build_log 2>&1
+cd $__ubdl_buildall_py2_workdir__
+
+echo "<<< BUILD LARFLOW >>>"
+mkdir -p larflow/build
+cd larflow
+source configure.sh
+cd build
+cmake ../
+make install >> $build_log 2>&1
 cd $__ubdl_buildall_py2_workdir__
 
 echo "built ubdl modules"
