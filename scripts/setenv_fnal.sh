6e77be916c9a9776b12fcd2691d474849e44a767
CVMFS UBOONE Account <cvmfsuboone@oasiscfs02.fnal.gov>
2019-08-07 15:04:10 -0500

    update table for working on oasis and grid

--- scripts/setenv_fnal.sh
+++ scripts/setenv_fnal.sh
@@ -17,7 +17,7 @@ setup opencv v3_1_0 -q e17
 setup python v2_7_14b
 setup cmake v3_13_2
 setup numpy v1_14_3 -q e17:p2714b:prof
-setup libtorch v1_0_1_nonuma -q e17:prof
+setup libtorch v1_0_1 -q e17:prof
 
 # SETUP BY UPS, need slightly different version of variables for ubdl repos
 export OPENCV_INCDIR=${OPENCV_INC}
