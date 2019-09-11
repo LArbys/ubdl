a1cb06093445eb49761867d40b9609bde8e72fe2
jmills09 <jmills09@tufts.edu>
2019-09-11 16:53:14 +0000

    update to submodules

--- buildall_py2.sh
+++ buildall_py2.sh
@@ -4,43 +4,43 @@ __ubdl_buildall_py2_workdir__=$PWD
 
 build_log=${__ubdl_buildall_py2_workdir__}/build.log
 
-echo "<<< BUILD LARLITE >>>"
-cd larlite
-make >& $build_log 2>&1
-echo "<<< BUILD LARLITE/UserDev/BasicTool >>>"
-cd UserDev/BasicTool
-make -j4 >> $build_log 2>&1
-echo "<<< BUILD LARLITE/UserDev/SelectionTool/LEEPreCuts >>>"
-cd ../SelectionTool/LEEPreCuts
-git submodule init
-git submodule update
-make -j4 >> $build_log 2>&1
-cd $__ubdl_buildall_py2_workdir__
-
-echo "<<< BUILD GEO2D >>>"
-cd Geo2D
-make -j4 >> $build_log 2>&1
-cd $__ubdl_buildall_py2_workdir__
-
-echo "<<< BUILD LAROPENCV >>>"
-cd LArOpenCV
-make -j4 >> $build_log 2>&1
-cd $__ubdl_buildall_py2_workdir__
-
-echo "<<< BUILD LARCV >>>"
-cd larcv
-mkdir -p build
-cd build
-cmake -DUSE_PYTHON2=ON -DUSE_OPENCV=ON -DON_FNAL=ON ../
-make install >> $build_log 2>&1
-cd $__ubdl_buildall_py2_workdir__
-
-echo "<<< BUILD CILANTRO >>>"
-cd cilantro
-mkdir -p build
-cd build
-cmake ../
-make >> $build_log 2>&1
+# echo "<<< BUILD LARLITE >>>"
+# cd larlite
+# make >& $build_log 2>&1
+# echo "<<< BUILD LARLITE/UserDev/BasicTool >>>"
+# cd UserDev/BasicTool
+# make -j4 >> $build_log 2>&1
+# echo "<<< BUILD LARLITE/UserDev/SelectionTool/LEEPreCuts >>>"
+# cd ../SelectionTool/LEEPreCuts
+# git submodule init
+# git submodule update
+# make -j4 >> $build_log 2>&1
+# cd $__ubdl_buildall_py2_workdir__
+#
+# echo "<<< BUILD GEO2D >>>"
+# cd Geo2D
+# make -j4 >> $build_log 2>&1
+# cd $__ubdl_buildall_py2_workdir__
+#
+# echo "<<< BUILD LAROPENCV >>>"
+# cd LArOpenCV
+# make -j4 >> $build_log 2>&1
+# cd $__ubdl_buildall_py2_workdir__
+#
+# echo "<<< BUILD LARCV >>>"
+# cd larcv
+# mkdir -p build
+# cd build
+# cmake -DUSE_PYTHON2=ON -DUSE_OPENCV=ON -DON_FNAL=ON ../
+# make install >> $build_log 2>&1
+# cd $__ubdl_buildall_py2_workdir__
+#
+# echo "<<< BUILD CILANTRO >>>"
+# cd cilantro
+# mkdir -p build
+# cd build
+# cmake ../
+# make >> $build_log 2>&1
 cd $__ubdl_buildall_py2_workdir__
 
 echo "<<< BUILD UBLARCVAPP >>>"
@@ -49,7 +49,7 @@ cd ublarcvapp
 source configure.sh
 cd build
 cmake -DUSE_OPENCV=ON ../
-make install >> $build_log 2>&1
+make install 
 cd $__ubdl_buildall_py2_workdir__
 
 echo "<<< BUILD LARFLOW >>>"
