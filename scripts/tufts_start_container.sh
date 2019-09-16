56eb2b890aa8e1bd6042ef44c3cbc4f15e7953ed
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2019-09-16 21:50:16 +0000

    restore build script

--- scripts/tufts_start_container.sh
+++ scripts/tufts_start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_deps_py2_082619.simg
+container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_deps_py2_091319.simg
 
 module load singularity
 singularity shell --nv $container
