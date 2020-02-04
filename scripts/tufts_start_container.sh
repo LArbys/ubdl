30b6aeef90f5fcfa07c6c7d2e507f8c2178de5e7
jmills09 <jmills09@tufts.edu>
2020-02-04 14:28:15 -0500

    Updated container path, and reco3d packages

--- scripts/tufts_start_container.sh
+++ scripts/tufts_start_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_deps_py2_091319.simg
-
+#This container seems to be gone -josh
+#container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_deps_py2_091319.simg
+container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/singularity_ubdl_deps_py2_10022019.simg
 module load singularity
 singularity shell --nv $container
