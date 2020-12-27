b6912ee1906a68c3730eb67fc2d3139d84f1837f
taritree <taritree.wongjirad@gmail.com>
2020-12-27 12:12:34 -0500

    update to right python3 container

--- scripts/tufts_start_container_py3.sh
+++ scripts/tufts_start_container_py3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/ubdldeps_u20.02_py3.simg
+container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/ubdl_py3deps_u18.04_cu11_pytorch1.7.1.simg
 module load singularity
 singularity shell --nv $container
