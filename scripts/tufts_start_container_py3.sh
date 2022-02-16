ec28f3dbc6ba437fd81bee637d399167670fef46
twongjirad <taritree.wongjirad@gmail.com>
2022-02-16 15:20:17 -0500

    container py3 script points to most up to date container. larflow bumped for mlreco keypoint functions

--- scripts/tufts_start_container_py3.sh
+++ scripts/tufts_start_container_py3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/ubdl_py3deps_u18.04_cu11_pytorch1.7.1.simg
-container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/ubdldeps_u20.02_pytorch1.9_py3.simg
+#container=/cluster/tufts/wongjiradlab/larbys/larbys-containers/ubdldeps_u20.02_pytorch1.9_py3.simg
+container=/cluster/tufts/wongjiradlabnu//larbys/larbys-container/singularity_minkowskiengine_u20.04.cu111.torch1.9.0.sif
 module load singularity/3.5.3
-singularity shell --nv --bind /cluster/tufts/:/cluster/tufts/ $container
+singularity shell --nv --bind /cluster/tufts/:/cluster/tufts/,/tmp:/tmp $container
