539722b7e3e802243aaa2308f231ebaa0f475cbc
Taritree Wongjirad <twongjirad@gmail.com>
2022-02-21 18:09:29 -0500

    update root and opencv locations for rebuilt meitner

--- setenv_py3.sh
+++ setenv_py3.sh
@@ -19,12 +19,12 @@ elif [ $MACHINE == "meitner" ]
 then
     echo "SETUP MEITNER"
 
-    source /usr/local/root6/bin/thisroot.sh
+    source /usr/local/root/v6.24.06/bin/thisroot.sh
 
     export CUDA_HOME=/usr/local/cuda-10.0
     [[ ":$LD_LIBRARY_PATH:" != *":${CUDA_HOME}/lib64:"* ]] && export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}"
 
-    export OPENCV_INCDIR=/usr/include
+    export OPENCV_INCDIR=/usr/local/include/opencv4
     export OPENCV_LIBDIR=/usr/local/lib    
 
 elif [ $MACHINE == "goeppert" ]
